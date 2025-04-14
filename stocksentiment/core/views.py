147707fa9a15400dadb5f04c468165e72b39f512
Sagnik Chakraborty <sagnify2022@gmail.com>
2025-04-14 21:26:16 +0530

    added gitignore

--- stocksentiment/core/views.py
+++ stocksentiment/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from scripts import fetch_analyze as fa
+from models import CompanySentiment
+from django.http import JsonResponse
+
 
 # Create your views here.
+# def index(request):
+    

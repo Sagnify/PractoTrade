5eace524be345580393bf9fb48d012ad3444fb24
Tautik05 <tautiksinharoy@gmail.com>
2025-04-15 21:53:12 +0530

    added redis container for redis

--- stocksentiment/core/task.py
+++ stocksentiment/core/task.py
@@ -4,7 +4,7 @@ import time
 
 @shared_task
 def handle_sleep():
-    print("Sleeping for 10 seconds...")
+    print("Sleeping for 20 seconds...")
     time.sleep(20)
 
 # @shared_task(bind=True, name='handle_sleep_task')

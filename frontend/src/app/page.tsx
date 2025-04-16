9d29fbe2b824bfbd8929cfd26468fd0219304f6d
captainTigerYT <raunakmanna43@gmail.com>
2025-04-17 01:25:02 +0530

    added basic card layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
+import CompanyCards from "@/components/CompanyCards";
 import React from "react";
 
 const page = () => {
-  return <div></div>;
+  return <div>
+    <CompanyCards />
+  </div>;
 };
 
 export default page;
